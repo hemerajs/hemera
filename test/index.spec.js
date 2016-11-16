64edc071adbd5946b07c248a3c306c25c924f044
Dustin Deus <deusdustin@gmail.com>
2016-11-16 20:23:50 +0100

    add tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -31,24 +31,137 @@ describe('Basic', function () {
       nats
     });
 
-    hemera.add({
-      topic: 'math',
-      cmd: 'add'
-    }, (resp, cb) => {
+    hemera.ready(() => {
 
-      cb({ result: resp.a + resp.b });
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, (resp, cb) => {
+
+        cb({
+          result: resp.a + resp.b
+        });
+      });
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, (err, resp) => {
+
+        expect(err).not.to.be.exists();
+        expect(resp.result).to.be.equals(3);
+
+        hemera.close();
+        done();
+      });
+
     });
 
-    hemera.act({
-      topic: 'math',
-      cmd: 'add',
-      a: 1,
-      b: 2
-    }, (resp) => {
+  });
 
-      expect(resp.result).to.be.equals(3);
+  it('Should be able to call a handler by different patterns', function (done) {
 
-      done();
+    const nats = require('nats').connect(authUrl);
+    const hemera = new Hemera({
+      nats
+    });
+
+    hemera.ready(() => {
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, (resp, cb) => {
+
+        cb({
+          result: resp.a + resp.b
+        });
+      });
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'sub'
+      }, (resp, cb) => {
+
+        cb({
+          result: resp.a - resp.b
+        });
+      });
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, (err, resp) => {
+
+        expect(err).not.to.be.exists();
+        expect(resp.result).to.be.equals(3);
+
+        hemera.act({
+          topic: 'math',
+          cmd: 'sub',
+          a: 2,
+          b: 2
+        }, (err, resp) => {
+
+          expect(err).not.to.be.exists();
+          expect(resp.result).to.be.equals(0);
+          hemera.close();
+          done();
+        });
+      });
+
+    });
+
+  });
+
+});
+
+
+describe('Timeouts', function () {
+
+  var PORT = 6242;
+  var flags = ['--user', 'derek', '--pass', 'foobar'];
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT;
+  var noAuthUrl = 'nats://localhost:' + PORT;
+  var server;
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = nsc.start_server(PORT, flags, done);
+  });
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill();
+  });
+
+  it('Should be able to check for an error when we get no answer back within the timeout limit', function (done) {
+
+    const nats = require('nats').connect(authUrl);
+    const hemera = new Hemera({
+      nats,
+      timeout: 200
+    });
+
+    hemera.ready(() => {
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, (err, resp) => {
+
+        expect(err).to.be.exists();
+        expect(resp).not.to.be.exists();
+        hemera.close();
+        done();
+      });
+
     });
 
   });
