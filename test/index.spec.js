884da5b6e87681a71f828d3ca47af2ce350be1b4
Dustin Deus <deusdustin@gmail.com>
2016-11-19 17:01:41 +0100

    increase coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -711,4 +711,139 @@ describe('Plugin interface', function () {
 
   })
 
+  it('Should be able to check duplicate registered plugins', function (done) {
+
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera({
+      timeout: 200
+    })
+
+    Hemera.transport = nats
+
+    hemera.ready(() => {
+
+      let pluginOptions = {
+        a: '1'
+      }
+
+      //Plugin
+      let plugin = function (options) {
+
+        let hemera = this
+
+        hemera.add({
+          topic: 'math',
+          cmd: 'add'
+        }, (resp, cb) => {
+
+          cb(null, {
+            result: resp.a + resp.b
+          })
+        })
+
+        return {
+          name: 'myPlugin'
+        }
+
+      }
+
+      //Plugin
+      let plugin2 = function (options) {
+
+        let hemera = this
+
+        hemera.add({
+          topic: 'math',
+          cmd: 'sub'
+        }, (resp, cb) => {
+
+          cb(null, {
+            result: resp.a + resp.b
+          })
+        })
+
+        return {
+          name: 'myPlugin'
+        }
+
+      }
+
+      try {
+
+        hemera.use(plugin, pluginOptions)
+        hemera.use(plugin2, pluginOptions)
+
+      } catch (e) {
+
+        expect(e.name).to.be.equals('HemeraError')
+        expect(e.message).to.be.equals('Plugin is already registered')
+        hemera.close()
+        done()
+      }
+
+    })
+
+  })
+
 })
+
+describe('Logging interface', function () {
+
+  var PORT = 6242
+  var flags = ['--user', 'derek', '--pass', 'foobar']
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT
+  var noAuthUrl = 'nats://localhost:' + PORT
+  var server
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = nsc.start_server(PORT, flags, done)
+  })
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill()
+  })
+
+  it('Should be able to log', function (done) {
+
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera({
+      timeout: 200,
+      debug: true
+    })
+
+    Hemera.transport = nats
+
+    hemera.ready(() => {
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, (resp, cb) => {
+
+        cb(null, {
+          result: resp.a + resp.b
+        })
+      })
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, (err, resp) => {
+
+        expect(err).to.be.not.exists()
+        expect(resp).not.to.be.equals(3)
+        hemera.close()
+        done()
+      })
+
+    })
+
+  })
+
+});
