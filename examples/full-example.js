e122c652382a10b4857950692032c6a669c00c20
Dustin Deus <deusdustin@gmail.com>
2016-11-19 04:55:41 +0100

    ...

--- examples/full-example.js
+++ examples/full-example.js
@@ -1,65 +1,41 @@
 'use strict'
 
 const Hemera = require('./../')
-const nats = require('nats').connect()
+const nats = require ('nats').connect()
 
-const hemera = new Hemera({
-  debug: true
-})
+const hemera = new Hemera({ debug: true })
 
 Hemera.transport = nats
 
 hemera.ready(() => {
 
   /**
-   * Your Implementations
-   */
-  hemera.add({
-    topic: 'math',
-    cmd: 'add'
-  }, (resp, cb) => {
-
-    console.log('Meta', resp.meta$);
+  * Your Implementations
+  */
+  hemera.add({ topic: 'math', cmd: 'add' }, (resp, cb) => {
 
     cb(null, resp.a + resp.b)
   })
 
+  hemera.add({ topic: 'math', cmd: 'sub' }, (resp, cb) => {
+
+    cb(null, resp.a - resp.b)
+  })
+
   /**
-   * Call them
-   */
-  hemera.act({
-    topic: 'math',
-    cmd: 'add',
-    a: 1,
-    b: 2
-  }, function (err, resp) {
-
-    //Set context
-    this.context$.token = '123';
-
-    //Transfer metadata
-    this.meta$.test = 'meta';
-
-    this.act({
-      topic: 'math',
-      cmd: 'add',
-      a: 1,
-      b: resp
-    }, function (err, resp) {
-
-      console.log('Context', this.meta$);
-
-      this.act({
-        topic: 'math',
-        cmd: 'add',
-        a: 1,
-        b: resp
-      }, function (err, resp) {
-
-        console.log('Result', resp)
-      })
+  * Call them
+  */
+  hemera.act({ topic: 'math', cmd: 'add', a: 1, b: 2 }, function (err, resp) {
+    
+    hemera.act({ topic: 'math', cmd: 'add', a: 1, b: resp }, function (err, resp) {
+    
+      console.log('Result', resp)
     })
+  })
 
+  hemera.act({ topic: 'math', cmd: 'add', a: 1, b: 20 }, (err, resp) => {
+    
+    console.log('Result', resp)
   })
 
 })
