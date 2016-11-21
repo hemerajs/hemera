7188b3de6989b2f8172d06990ae95b355e8ac475
Dustin Deus <deusdustin@gmail.com>
2016-11-21 01:20:37 +0100

    add tests fixed passing metadata

--- test/index.spec.js
+++ test/index.spec.js
@@ -941,3 +941,141 @@ describe('Logging interface', function () {
   })
 
 });
+
+describe('Metadata', function () {
+
+  var PORT = 6242
+  var flags = ['--user', 'derek', '--pass', 'foobar']
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT
+  var noAuthUrl = 'nats://localhost:' + PORT
+  var server
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = nsc.start_server(PORT, flags, done)
+  })
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill()
+  })
+
+  it('Should be able to pass metadata', function (done) {
+
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera(nats)
+
+    hemera.ready(() => {
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, function (resp, cb) {
+
+        expect(resp.meta$).to.be.equals('test')
+
+        cb(null, {
+          result: resp.a + resp.b
+        })
+      })
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2,
+        meta$: 'test'
+      }, function (err, resp) {
+
+        expect(err).to.be.not.exists()
+        expect(resp).not.to.be.equals(3)
+
+        this.act({
+          topic: 'math',
+          cmd: 'add',
+          a: 1,
+          b: 2
+        }, function (err, resp) {
+
+          expect(err).to.be.not.exists()
+          expect(resp).not.to.be.equals(3)
+          hemera.close()
+          done()
+        })
+      })
+
+    })
+
+  })
+
+})
+
+describe('Context', function () {
+
+  var PORT = 6242
+  var flags = ['--user', 'derek', '--pass', 'foobar']
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT
+  var noAuthUrl = 'nats://localhost:' + PORT
+  var server
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = nsc.start_server(PORT, flags, done)
+  })
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill()
+  })
+
+  it('Should be able to create a context', function (done) {
+
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera(nats)
+
+    hemera.ready(() => {
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, function (resp, cb) {
+
+        cb(null, {
+          result: resp.a + resp.b
+        })
+      })
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2,
+        context$: 'test'
+      }, function (err, resp) {
+
+        expect(this.context$).to.be.equals('test')
+        expect(err).to.be.not.exists()
+        expect(resp).not.to.be.equals(3)
+
+        this.act({
+          topic: 'math',
+          cmd: 'add',
+          a: 1,
+          b: 2
+        }, function (err, resp) {
+
+          expect(this.context$).to.be.equals('test')
+          expect(err).to.be.not.exists()
+          expect(resp).not.to.be.equals(3)
+          hemera.close()
+          done()
+        })
+      })
+
+    })
+
+  })
+
+});
