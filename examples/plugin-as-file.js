71c4807ac3ce3a0e3912fe0803b2e933bab48071
Dustin Deus <deusdustin@gmail.com>
2016-11-25 21:37:41 +0100

    add example for plugin as own file clean code

---
+++ examples/plugin-as-file.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+exports.plugin = function myPlugin(options) {
+
+  var hemera = this
+
+  hemera.add({
+    topic: 'math',
+    cmd: 'add'
+  }, (resp, cb) => {
+
+    cb(null, resp.a + resp.b)
+  })
+
+}
+
+exports.options = {
+  a: 1
+}
+
+exports.attributes = {
+  name: 'myPlugin'
+}
+
+
+/**
+ * Usage
+ *
+
+  const Hemera = require('./../')
+  const nats = require('nats').connect()
+
+  const hemera = new Hemera(nats, { logLevel: 'info' })
+
+  hemera.ready(() => {
+
+    const plugin = require('./plugin-as-file')
+    plugin.options.a = 10
+
+    hemera.use(plugin)
+
+  })
+
+ */
