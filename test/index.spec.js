99cbaa772ffa66ddd2e752de6350ca088c64c5d4
Dustin Deus <deusdustin@gmail.com>
2016-12-01 21:39:59 +0100

    add test for tracing, fixed typo

--- test/index.spec.js
+++ test/index.spec.js
@@ -1223,6 +1223,138 @@ describe('Context', function () {
 
 })
 
+describe('Tracing', function () {
+
+  var PORT = 6242
+  var flags = ['--user', 'derek', '--pass', 'foobar']
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT
+  var noAuthUrl = 'nats://localhost:' + PORT
+  var server
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = nsc.start_server(PORT, flags, done)
+  })
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill()
+  })
+
+  it('Should set correct request parentId$ and request$ context', function (done) {
+
+    /**
+     * math:add-->math:sub
+     *            math:add
+     *            math:add-->
+     *
+     */
+
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera(nats)
+
+    hemera.ready(() => {
+
+      expect(this.parentId$).to.be.not.exists()
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, function (resp, cb) {
+
+        expect(this.parentId$).to.be.exists()
+
+        cb(null, resp.a + resp.b)
+      })
+
+      hemera.add({
+        topic: 'math',
+        cmd: 'sub'
+      }, function (resp, cb) {
+
+        let r1 = this.requestId$
+
+        expect(this.parentId$).to.be.exists()
+
+        this.act({
+          topic: 'math',
+          cmd: 'add',
+          a: 1,
+          b: 2
+        }, function (err, resp2) {
+
+          let r2 = this.requestId$
+
+          expect(this.parentId$).to.be.equals(r1)
+
+          expect(this.request$.startTime).to.be.a.number()
+          expect(this.request$.endTime).to.be.a.number()
+          expect(this.request$.transportLatency).to.be.a.number()
+          expect(this.request$.duration).to.be.a.number()
+
+          this.act({
+            topic: 'math',
+            cmd: 'add',
+            a: 10,
+            b: 2
+          }, function (err, resp2) {
+
+            expect(this.parentId$).to.be.equals(r2)
+
+            expect(this.request$.startTime).to.be.a.number()
+            expect(this.request$.endTime).to.be.a.number()
+            expect(this.request$.transportLatency).to.be.a.number()
+            expect(this.request$.duration).to.be.a.number()
+
+            cb(null, resp.a - resp.b)
+          })
+
+        })
+      })
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, function (err, resp) {
+
+        let r1 = this.requestId$
+
+        expect(this.request$.id).to.be.string()
+        expect(this.request$.startTime).to.be.a.number()
+        expect(this.request$.endTime).to.be.a.number()
+        expect(this.request$.transportLatency).to.be.a.number()
+        expect(this.request$.duration).to.be.a.number()
+
+        this.act({
+          topic: 'math',
+          cmd: 'sub',
+          a: 1,
+          b: resp
+        }, function (err, resp) {
+
+          expect(this.parentId$).to.be.equals(r1)
+          expect(this.request$.id).to.be.string()
+          expect(this.request$.parentId).to.be.a.string()
+          expect(this.request$.startTime).to.be.a.number()
+          expect(this.request$.endTime).to.be.a.number()
+          expect(this.request$.transportLatency).to.be.a.number()
+          expect(this.request$.duration).to.be.a.number()
+
+          hemera.close()
+          done()
+
+        })
+      })
+
+    })
+
+  })
+
+})
+
 describe('Memory leak test', function () {
 
   var PORT = 6242
