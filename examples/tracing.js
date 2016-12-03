68e17583ac6e422bba2cb84478580d7ce5ed447f
Dustin Deus <deusdustin@gmail.com>
2016-12-04 00:48:59 +0100

    clean example

--- examples/tracing.js
+++ examples/tracing.js
@@ -30,15 +30,20 @@ hemera.ready(() => {
   }, function (resp, cb) {
 
     let r1 = this.requestId$
+
+    this.act({
+      topic: 'math',
+      cmd: 'add',
+      a: 1,
+      b: 2
+    })
+
     this.act({
       topic: 'math',
       cmd: 'add',
       a: 1,
       b: 2
     }, function (err, resp2) {
-
-      let r2 = this.requestId$
-      this.log.info('ParentId Level1 Match =', this.parentId$ === r1, this.request$);
 
       this.act({
         topic: 'math',
@@ -47,7 +52,6 @@ hemera.ready(() => {
         b: 2
       }, function (err, resp2) {
 
-        this.log.info('ParentId Level2 Match =', this.parentId$ === r2, this.request$);
         cb(null, resp.a - resp.b)
       });
     });
@@ -60,18 +64,18 @@ hemera.ready(() => {
     topic: 'math',
     cmd: 'add',
     a: 1,
-    b: 2
+    b: 20
   }, function (err, resp) {
     let r1 = this.requestId$
 
     this.act({
       topic: 'math',
       cmd: 'sub',
-      a: 1,
+      a: 100,
       b: resp
     }, function (err, resp) {
 
-      this.log.info('ParentId Match =', r1 === this.parentId$)
+      this.log.info('Finished', resp)
     })
   })
 
