7cb75c54ab42e417162dd8a44ad76ddaf22de70d
Dustin Deus <deusdustin@gmail.com>
2016-12-08 00:18:28 +0100

    better zipkin example

--- examples/zipkin-tracing.js
+++ examples/zipkin-tracing.js
@@ -65,46 +65,55 @@ hemera.ready(() => {
    * Your Implementations
    */
   hemera.add({
-    topic: 'math',
-    cmd: 'add'
+    topic: 'auth',
+    cmd: 'signup',
   }, function (resp, cb) {
 
+    let userId = 1
+
     this.act({
-      topic: 'math',
-      cmd: 'sub',
-      a: 100,
-      b: 20
+      topic: 'email',
+      cmd: 'send',
+      email: resp.email,
+      message: 'Welcome!'
     }, function (err, resp) {
 
       this.act({
-        topic: 'math',
-        cmd: 'sub',
-        a: 100,
-        b: 50
+        topic: 'payment',
+        cmd: 'process',
+        userId: userId
       }, function (err, resp) {
 
-        cb(null, resp)
+        cb(null, true)
       })
     })
 
   })
 
   hemera.add({
-    topic: 'math',
-    cmd: 'sub'
+    topic: 'payment',
+    cmd: 'process'
   }, function (resp, cb) {
 
-    cb(null, resp.a - resp.b)
+    cb(null, true)
+  })
+
+  hemera.add({
+    topic: 'email',
+    cmd: 'send'
+  }, function (resp, cb) {
+
+    cb(null, true)
   })
 
   /**
    * Call them
    */
   hemera.act({
-    topic: 'math',
-    cmd: 'add',
-    a: 1,
-    b: 20
+    topic: 'auth',
+    cmd: 'signup',
+    email: 'peter@gmail.com',
+    password: '1234'
   }, function (err, resp) {
 
     this.log.info('Finished', resp)
