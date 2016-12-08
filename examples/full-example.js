07cb75d2d49b51780d711765773e937b75f6eb85
Russlan Akiev <russlan.akiev@netzkern.de>
2016-12-08 18:08:26 +0100

    Formatting

--- examples/full-example.js
+++ examples/full-example.js
@@ -1,39 +1,61 @@
 'use strict'
 
 const Hemera = require('./../')
-const nats = require ('nats').connect()
+const nats = require('nats').connect()
 
-const hemera = new Hemera(nats, { logLevel: 'info' })
+const hemera = new Hemera(nats, {
+  logLevel: 'info'
+})
 
-hemera.ready(() => {    
+hemera.ready(() => {
 
   /**
-  * Your Implementations
-  */
-  hemera.add({ topic: 'math', cmd: 'add' }, (resp, cb) => {
+   * Your Implementations
+   */
+  hemera.add({
+    topic: 'math',
+    cmd: 'add'
+  }, (resp, cb) => {
 
     cb(null, resp.a + resp.b)
   })
 
-  hemera.add({ topic: 'math', cmd: 'sub' }, (resp, cb) => {
+  hemera.add({
+    topic: 'math',
+    cmd: 'sub'
+  }, (resp, cb) => {
 
     cb(null, resp.a - resp.b)
   })
 
   /**
-  * Call them
-  */
-  hemera.act({ topic: 'math', cmd: 'add', a: 1, b: 2 }, function (err, resp) {
+   * Call them
+   */
+  hemera.act({
+    topic: 'math',
+    cmd: 'add',
+    a: 1,
+    b: 2
+  }, function (err, resp) {
 
-    hemera.act({ topic: 'math', cmd: 'add', a: 1, b: resp }, function (err, resp) {
+    hemera.act({
+      topic: 'math',
+      cmd: 'add',
+      a: 1,
+      b: resp
+    }, function (err, resp) {
 
       this.log.info('Result', resp)
     })
   })
 
-  hemera.act({ topic: 'math', cmd: 'add', a: 1, b: 20 }, function(err, resp) {
-    
+  hemera.act({
+    topic: 'math',
+    cmd: 'add',
+    a: 1,
+    b: 20
+  }, function (err, resp) {
+
     this.log.info('Result', resp)
   })
-
 })
--- examples/server.js
+++ examples/server.js
@@ -1,18 +1,22 @@
 'use strict'
 
 const Hemera = require('./../')
-const nats = require ('nats').connect()
+const nats = require('nats').connect()
 
-const hemera = new Hemera(nats, { logLevel: 'info' })
+const hemera = new Hemera(nats, {
+  logLevel: 'info'
+})
 
 hemera.ready(() => {
 
   /**
-  * Your Implementations
-  */
-  hemera.add({ topic: 'math', cmd: 'add' }, function (resp, cb) {
+   * Your Implementations
+   */
+  hemera.add({
+    topic: 'math',
+    cmd: 'add'
+  }, function (resp, cb) {
 
     cb(null, resp.a + resp.b)
   })
-
 })
