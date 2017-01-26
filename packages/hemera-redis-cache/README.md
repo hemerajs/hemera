54723d2b4c269ea6eb9c1d242e334a0a9109f213
Dustin Deus <deusdustin@gmail.com>
2017-01-26 14:26:34 +0100

    fixed deps, add redis-cache plugin

---
+++ packages/hemera-redis-cache/README.md
@@ -0,0 +1,5 @@
+# Hemera-redis-cache package
+
+[![npm](https://img.shields.io/npm/v/hemera-redis-cache.svg?maxAge=3600)](https://www.npmjs.com/package/hemera-redis-cache)
+
+This is a plugin to use [Redis](https://redis.io/) as caching layer with Hemera. Underlying driver [Node Redis](https://github.com/NodeRedis/node_redis)
---
+++ packages/hemera-redis-cache/index.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const Redis = require('redis')
+
+exports.plugin = function hemeraRedisCache(options) {
+
+  const hemera = this
+  const client = Redis.createClient(options.redis)
+  const topic = 'redis-cache'
+
+  client.on('ready', function () {
+
+    hemera.log.info('Redis Cache is ready')
+  })
+
+  client.on('end', function () {
+
+    hemera.log.warn('Redis client connection closed')
+  })
+
+  client.on('reconnecting', function (msg) {
+
+    hemera.log.info('Redis client is reconnecting', msg)
+  })
+
+  client.on('warning', function (msg) {
+
+    hemera.log.warn('Redis client warning', msg)
+  })
+
+  client.on('error', function (err) {
+
+    hemera.log.fatal(err)
+    hemera.fatal()
+  })
+
+
+  hemera.add({
+    topic,
+    cmd: 'set'
+  }, function (req, cb) {
+
+    client.set(req.key, req.val, cb)
+
+  })
+
+  hemera.add({
+    topic,
+    cmd: 'get'
+  }, function (req, cb) {
+
+    client.get(req.key, cb)
+
+  })
+
+  hemera.add({
+    topic,
+    cmd: 'hmset'
+  }, function (req, cb) {
+
+    client.hmset(req.key, req.data, cb)
+
+  })
+
+  hemera.add({
+    topic,
+    cmd: 'hgetall'
+  }, function (req, cb) {
+
+    client.hgetall(req.key, cb)
+
+  })
+
+}
+
+exports.options = {
+  redis: null
+}
+
+exports.attributes = {
+  name: 'hemera-redis-cache'
+}
