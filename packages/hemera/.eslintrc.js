825b0203c13da9261e7f17b2176341c0a756a312
Dustin Deus <deusdustin@gmail.com>
2017-02-17 21:14:52 +0100

    add mocha as eslint env

--- packages/hemera/.eslintrc.js
+++ packages/hemera/.eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  "parser": "babel-eslint",
   "env": {
     "es6": true,
-    "node": true
+    "node": true,
+    "mocha": true
   },
-  "plugins": [
-    "flowtype"
-  ],
+  "plugins": [],
   "extends": [
-    "standard"
+    "eslint:recommended"
   ],
   "parserOptions": {
     "sourceType": "module"
   },
   // see http://eslint.org/docs/rules/
   "rules": {
-    "indent": ["error", 2],
-    "quotes": ["error", "single"]
+    "indent": [ "error", 2 ],
+    "quotes": [ "error", "single" ]
   }
 };
