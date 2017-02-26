4f0807506ae60800822f3f971bea3fb261185abc
Dustin Deus <deusdustin@gmail.com>
2017-02-26 12:03:25 +0100

    add test for circular references

--- test/hemera/index.spec.js
+++ test/hemera/index.spec.js
@@ -2764,7 +2764,9 @@ describe('Response error events', function () {
   it('client response error result', function (done) {
     const nats = require('nats').connect(authUrl)
 
-    const hemera = new Hemera(nats, { crashOnFatal: false })
+    const hemera = new Hemera(nats, {
+      crashOnFatal: false
+    })
 
     hemera.ready(() => {
       hemera.on('clientResponseError', function (err) {
@@ -2827,6 +2829,61 @@ describe('Response error events', function () {
         cmd: 'add',
         a: 1,
         b: 2
+      })
+    })
+  })
+})
+
+describe('Default JSON encoder', function () {
+  var PORT = 6242
+  var flags = ['--user', 'derek', '--pass', 'foobar']
+  var authUrl = 'nats://derek:foobar@localhost:' + PORT
+  var server
+
+  // Start up our own nats-server
+  before(function (done) {
+    server = HemeraTestsuite.start_server(PORT, flags, done)
+  })
+
+  // Shutdown our server after we are done
+  after(function () {
+    server.kill()
+  })
+
+  it('Should be able to handle circular references', function (done) {
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera(nats)
+
+    hemera.ready(() => {
+      hemera.add({
+        topic: 'circular',
+        cmd: 'test'
+      }, (resp, cb) => {
+        const fixture = {
+          name: 'Tywin Lannister',
+          child: {
+            name: 'Tyrion Lannister'
+          }
+        }
+        fixture.child.dinklage = fixture.child
+        cb(null, fixture)
+      })
+
+      hemera.act({
+        topic: 'circular',
+        cmd: 'test'
+      }, function (err, res) {
+        expect(err).to.be.not.exists()
+        expect(res).to.be.equals({
+          name: 'Tywin Lannister',
+          child: {
+            name: 'Tyrion Lannister',
+            dinklage: '[Circular]'
+          }
+        })
+        hemera.close()
+        done()
       })
     })
   })
