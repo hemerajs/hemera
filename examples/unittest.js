bd2676b0d88a13cfdfcefd89197a49d232938e6c
Dustin Deus <deusdustin@gmail.com>
2017-03-14 21:57:44 +0100

    imrpove unittest example

--- examples/unittest.js
+++ examples/unittest.js
@@ -1,35 +1,44 @@
+/**
+ * Run mocha ./examples/unittest.js
+ */
+
 const Hemera = require('./../packages/hemera')
 const NatsStub = require('hemera-testsuite/natsStub')
-const Assert = require('assert')
+const Code = require('code')
+const expect = Code.expect
 
-const nats = new NatsStub()
-const hemera = new Hemera(nats, {
-  logLevel: 'info'
-})
+describe('Math', function () {
+  it('Should add two numbers', function (done) {
+    const nats = new NatsStub()
+    const hemera = new Hemera(nats, {
+      logLevel: 'info'
+    })
 
-hemera.ready(function () {
-  // Define server method
-  hemera.add({
-    topic: 'math',
-    cmd: 'add'
-  }, (req, cb) => {
-    cb(null, req.a + req.b)
-  })
+    hemera.ready(function () {
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, function (resp, cb) {
+        cb(null, resp.a + resp.b)
+      })
 
-  // get server method by pattern signature
-  const payload = hemera.router.lookup({
-    topic: 'math',
-    cmd: 'add'
-  })
+      // get server method by pattern signature
+      const payload = hemera.router.lookup({
+        topic: 'math',
+        cmd: 'add'
+      })
 
-  // pass arguments
-  const request = {
-    a: 1,
-    b: 2
-  }
-  // call action  but beware the scope is not set
-  payload.action(request, function (err, result) {
-    Assert(!err, 'Should not return an error')
-    Assert(result === 3, 'Should be 3')
+      // pass arguments
+      const request = {
+        a: 1,
+        b: 2
+      }
+      // call action  but beware the scope is not set
+      payload.action(request, function (err, result) {
+        expect(err).to.be.not.exists()
+        expect(result).to.be.equals(3)
+        done()
+      })
+    })
   })
 })
