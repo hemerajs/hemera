af80cd1e7cec17ceb36786509689caabfb090bad
dustin deus <dustin.deus@netzkern.de>
2017-03-21 13:43:39 +0100

    unit testing

--- examples/unittest.js
+++ examples/unittest.js
@@ -18,32 +18,31 @@ describe('Math', function () {
       logLevel: 'info'
     })
 
-    // stub act calls
-    Act.stub(hemera, { topic: 'math', cmd: 'sub', a: 10, b: 5 }, null, 5)
-    Act.stub(hemera, { topic: 'math', cmd: 'add', a: 1, b: 2 }, null, 3)
-
     hemera.ready(function () {
       hemera.add({
         topic: 'math',
         cmd: 'add'
       }, function (args, cb) {
-        this.act({ topic: 'math', cmd: 'sub', a: 10, b: 5 }, function (err, resp) {
-          cb(err, args.a + args.b + resp)
+        this.act({ topic: 'math', cmd: 'sub', a: 100, b: 50 }, function (err, resp) {
+          cb(err, args.a + args.b - resp)
         })
       })
       
+      // stub act calls
+      Act.stub(hemera, { topic: 'math', cmd: 'sub', a: 100, b: 50 }, null, 5)
+      Act.stub(hemera, { topic: 'math', cmd: 'add', a: 100, b: 200 }, null, 3)
       // Important stub when "add" was already added
       // Should execute the server method with the pattern topic:math,cmd:add,a:1,b:2"
-      Add.stub(hemera, { topic: 'math', cmd: 'add' }, { a: 1, b: 2 }, function (err, result) {
+      Add.run(hemera, { topic: 'math', cmd: 'add' }, { a: 100, b: 200 }, function (err, result) {
         expect(err).to.be.not.exists()
-        expect(result).to.be.equals(8)
+        expect(result).to.be.equals(295)
       })
 
       hemera.act({
         topic: 'math',
         cmd: 'add',
-        a: 1,
-        b: 2
+        a: 100,
+        b: 200
       }, function(err, result) {
         expect(err).to.be.not.exists()
         expect(result).to.be.equals(3)
