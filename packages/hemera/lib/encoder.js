16464c61c788284b2b6e6ba4251172228554ec78
Dustin Deus <deusdustin@gmail.com>
2017-04-15 22:13:42 +0200

    use fast-json-stringify package

--- packages/hemera/lib/encoder.js
+++ packages/hemera/lib/encoder.js
@@ -9,12 +9,14 @@
  *
  */
 
+const SafeStringify = require('fast-safe-stringify')
+
 class Encoder {
 
   static encode (msg) {
     try {
       return {
-        value: stringify(msg)
+        value: SafeStringify(msg)
       }
     } catch (error) {
       return {
@@ -22,53 +24,6 @@ class Encoder {
       }
     }
   }
-}
-
-function stringify (obj) {
-  decirc(obj, '', [], null)
-  return JSON.stringify(obj)
-}
-
-function Circle (val, k, parent) {
-  this.val = val
-  this.k = k
-  this.parent = parent
-  this.count = 1
-}
-
-Circle.prototype.toJSON = function toJSON () {
-  if (--this.count === 0) {
-    this.parent[this.k] = this.val
-  }
-  return '[Circular]'
-}
-
-function decirc (val, k, stack, parent) {
-  var keys, len, i
-
-  if (typeof val !== 'object' || val === null) {
-    // not an object, nothing to do
-    return
-  } else if (val instanceof Circle) {
-    val.count++
-    return
-  } else if (parent) {
-    if (~stack.indexOf(val)) {
-      parent[k] = new Circle(val, k, parent)
-      return
-    }
-  }
-
-  stack.push(val)
-  keys = Object.keys(val)
-  len = keys.length
-  i = 0
-
-  for (; i < len; i++) {
-    k = keys[i]
-    decirc(val[k], k, stack, val)
-  }
-  stack.pop()
 }
 
 module.exports = Encoder
