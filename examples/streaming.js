e1720ef8a19f2e9e92c7bc70d90e369167e43ad2
Dustin Deus <deusdustin@gmail.com>
2017-05-05 23:52:37 +0200

    add streaming example

---
+++ examples/streaming.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Hemera = require('./../packages/hemera')
+const nats = require('nats').connect()
+
+const hemera = new Hemera(nats, {
+  logLevel: 'silent'
+})
+
+const results = []
+
+hemera.ready(() => {
+  hemera.add({
+    topic: 'math',
+    cmd: 'add'
+  }, (resp, reply) => {
+    for (let i = 0; i < 1000; i++) {
+      reply(null, i)
+    }
+  })
+
+  hemera.act({
+    topic: 'math',
+    cmd: 'add',
+    maxMessages$: -1
+  }, function (err, resp) {
+    results.push(resp)
+    if (results.length === 1000) {
+      console.log('Received 1000 Messages!')
+      hemera.close()
+    }
+  })
+})
