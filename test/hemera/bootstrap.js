271b822079515d272140d67c8a05cd5d00b6007a
Dustin Deus <deusdustin@gmail.com>
2017-05-21 17:48:22 +0200

    exclude hemera-web from ci until we found a better solution

--- test/hemera/bootstrap.js
+++ test/hemera/bootstrap.js
@@ -5,7 +5,6 @@ global.HemeraUtil = require('../../packages/hemera/lib/util')
 global.Code = require('code')
 global.Sinon = require('sinon')
 global.HemeraTestsuite = require('hemera-testsuite')
-global.NodeVersion = require('node-version')
 global.expect = global.Code.expect
 global.UnauthorizedError = Hemera.createError('Unauthorized')
 
