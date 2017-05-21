d648ce2c3c6e6303e7d95d517f013895e3f3d166
Dustin Deus <deusdustin@gmail.com>
2017-05-21 15:32:32 +0200

    test default logger with real output

--- test/hemera/logging.spec.js
+++ test/hemera/logging.spec.js
@@ -54,4 +54,34 @@ describe('Logging interface', function () {
       })
     })
   })
+
+  it('Should be able to log with default logger', function (done) {
+    const nats = require('nats').connect(authUrl)
+    const hemera = new Hemera(nats, {
+      logLevel: 'info'
+    })
+
+    hemera.ready(() => {
+      hemera.add({
+        topic: 'math',
+        cmd: 'add'
+      }, (resp, cb) => {
+        cb(null, {
+          result: resp.a + resp.b
+        })
+      })
+
+      hemera.act({
+        topic: 'math',
+        cmd: 'add',
+        a: 1,
+        b: 2
+      }, (err, resp) => {
+        expect(err).to.be.not.exists()
+        expect(resp).not.to.be.equals(3)
+        hemera.close()
+        done()
+      })
+    })
+  })
 })
