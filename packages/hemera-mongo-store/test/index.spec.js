a8e062fee93d425175421fa58b5f2c04ab88913e
Jacob Bare <jacob@limit0.io>
2017-05-25 11:41:52 -0500

    Add exended json tests

--- packages/hemera-mongo-store/test/index.spec.js
+++ packages/hemera-mongo-store/test/index.spec.js
@@ -5,8 +5,34 @@ const HemeraMongoStore = require('./../index')
 const Nats = require('nats')
 const Code = require('code')
 const HemeraTestsuite = require('hemera-testsuite')
+const EJSON = require('mongodb-extended-json')
 
+const now = new Date()
 const expect = Code.expect
+
+function createExtendedData(mongodb) {
+  const oid = new mongodb.ObjectID('58c6c65ed78c6a977a0041a8')
+  return EJSON.serialize({
+    date: now,
+    objectId: oid,
+    ref: mongodb.DBRef('test', oid),
+  })
+}
+
+function testExtendedData(plugin, testCollection, id, done) {
+  const ObjectID = plugin.mongodb.ObjectID
+  const DBRef = plugin.mongodb.DBRef
+
+  plugin.db.collection(testCollection).findOne({
+    _id: new ObjectID(id)
+  }, (err, doc) => {
+    expect(err).to.be.null()
+    expect(doc.date).to.be.a.date()
+    expect(doc.objectId).to.be.an.instanceof(ObjectID)
+    expect(doc.ref).to.be.an.instanceof(DBRef)
+    done()
+  })
+}
 
 describe('Hemera-mongo-store', function () {
   let PORT = 6243
@@ -15,6 +41,8 @@ describe('Hemera-mongo-store', function () {
   const testCollection = 'test'
   let server
   let hemera
+  let plugin
+  let extendedData = {}
 
   before(function (done) {
     server = HemeraTestsuite.start_server(PORT, {}, () => {
@@ -28,6 +56,7 @@ describe('Hemera-mongo-store', function () {
         }
       })
       hemera.ready(() => {
+        plugin = hemera.exposition['hemera-mongo-store']
         hemera.act({
           topic,
           cmd: 'dropCollection',
@@ -79,6 +108,20 @@ describe('Hemera-mongo-store', function () {
     })
   })
 
+  it('create with extended json', function (done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: createExtendedData(plugin.mongodb)
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+      expect(resp._id).to.be.exists()
+      testExtendedData(plugin, testCollection, resp._id, done)
+    })
+  })
+
   it('update', function (done) {
     hemera.act({
       topic,
@@ -110,6 +153,64 @@ describe('Hemera-mongo-store', function () {
         expect(resp.name).to.be.exists()
 
         done()
+      })
+    })
+  })
+
+  it('update can query with extended json', function(done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: createExtendedData(plugin.mongodb)
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'update',
+        collection: testCollection,
+        data: {
+          $set: { name: 'foo' }
+        },
+        query: EJSON.serialize({ date: now }),
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        done()
+      })
+    })
+  })
+
+  it('update with extended json', function (done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: {
+        name: 'jacob'
+      }
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'update',
+        collection: testCollection,
+        data: {
+          $set: createExtendedData(plugin.mongodb)
+        },
+        query: {
+          name: 'jacob'
+        }
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        expect(resp._id).to.be.exists()
+        expect(resp.name).to.be.exists()
+        testExtendedData(plugin, testCollection, resp._id, done)
       })
     })
   })
@@ -147,6 +248,36 @@ describe('Hemera-mongo-store', function () {
     })
   })
 
+  it('updatebyId with extended json', function (done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: {
+        name: 'jacob'
+      }
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'updateById',
+        collection: testCollection,
+        data: {
+          $set: createExtendedData(plugin.mongodb)
+        },
+        id: resp._id
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        expect(resp._id).to.be.exists()
+        expect(resp.name).to.be.exists()
+        testExtendedData(plugin, testCollection, resp._id, done)
+      })
+    })
+  })
+
   it('remove', function (done) {
     hemera.act({
       topic,
@@ -171,6 +302,30 @@ describe('Hemera-mongo-store', function () {
         expect(resp).to.be.an.object()
         expect(resp.deletedCount).to.be.equals(1)
 
+        done()
+      })
+    })
+  })
+
+  it('remove can query with extended json', function(done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: createExtendedData(plugin.mongodb)
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'remove',
+        collection: testCollection,
+        query: EJSON.serialize({ date: now }),
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        expect(resp.deletedCount).to.be.at.least(1)
         done()
       })
     })
@@ -257,6 +412,31 @@ describe('Hemera-mongo-store', function () {
     })
   })
 
+  it('find can query with extended json', function(done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: createExtendedData(plugin.mongodb)
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'find',
+        collection: testCollection,
+        query: EJSON.serialize({ date: now }),
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp.result).to.be.an.array()
+        expect(resp.result[0]._id).to.be.exists()
+        expect(resp.result[0].date).to.be.exists()
+        done()
+      })
+    })
+  })
+
   it('find with pagination', function (done) {
     hemera.act({
       topic,
@@ -324,6 +504,70 @@ describe('Hemera-mongo-store', function () {
     })
   })
 
+  it('replace with extended json', function (done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: {
+        name: 'jacob'
+      }
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      const id = new plugin.mongodb.ObjectID(resp._id)
+
+      hemera.act({
+        topic,
+        cmd: 'replace',
+        collection: testCollection,
+        data: {
+          $set: createExtendedData(plugin.mongodb)
+        },
+        query: EJSON.serialize({ _id: id })
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        expect(resp.matchedCount).to.be.exists()
+        expect(resp.modifiedCount).to.be.exists()
+        expect(resp.upsertedCount).to.be.exists()
+        testExtendedData(plugin, testCollection, id, done)
+      })
+    })
+  })
+
+  it('replace can query with extended json', function(done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: createExtendedData(plugin.mongodb)
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'replace',
+        collection: testCollection,
+        data: {
+          $set: {
+            name: 'nadja'
+          }
+        },
+        query: EJSON.serialize({ date: now }),
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp).to.be.an.object()
+        expect(resp.matchedCount).to.be.exists()
+        expect(resp.modifiedCount).to.be.exists()
+        expect(resp.upsertedCount).to.be.exists()
+        done()
+      })
+    })
+  })
+
   it('replaceById', function (done) {
     hemera.act({
       topic,
@@ -349,6 +593,33 @@ describe('Hemera-mongo-store', function () {
         expect(resp._id).to.be.exists()
         expect(resp.name).to.be.exists()
         done()
+      })
+    })
+  })
+
+  it('replaceById with extended json', function (done) {
+    hemera.act({
+      topic,
+      cmd: 'create',
+      collection: testCollection,
+      data: {
+        name: 'jacob'
+      }
+    }, function (err, resp) {
+      expect(err).to.be.not.exists()
+      expect(resp).to.be.an.object()
+
+      hemera.act({
+        topic,
+        cmd: 'replaceById',
+        collection: testCollection,
+        data: createExtendedData(plugin.mongodb),
+        id: resp._id
+      }, function (err, resp) {
+        expect(err).to.be.not.exists()
+        expect(resp._id).to.be.exists()
+        expect(resp.name).to.be.exists()
+        testExtendedData(plugin, testCollection, resp._id, done)
       })
     })
   })
