a1c43ee1d13afbfe52026879f9c59c6b84662a36
Dustin Deus <deusdustin@gmail.com>
2017-06-05 14:04:13 +0200

    - Remove passing previous values to extensions - Remove passing extension index as parameter - Move reply logic from server response to reply class - Update tests

---
+++ packages/hemera/lib/reply.js
@@ -0,0 +1,85 @@
+'use strict'
+
+/**
+ * Copyright 2016-present, Dustin Deus (deusdustin@gmail.com)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+/**
+ *
+ *
+ * @class Reply
+ */
+class Reply {
+  /**
+   * Creates an instance of Reply.
+   * @param {any} request
+   * @param {any} response
+   * @param {any} extensionCallback
+   *
+   * @memberof Reply
+   */
+  constructor (request, response, extensionCallback) {
+    this._request = request
+    this._response = response
+    this.extensionCallback = extensionCallback
+  }
+
+  /**
+   *
+   *
+   * @param {any} payload
+   *
+   * @memberof Reply
+   */
+  set payload (value) {
+    this._response.payload = value
+  }
+
+  /**
+   *
+   *
+   * @readonly
+   *
+   * @memberof Reply
+   */
+  get payload () {
+    return this._response.payload
+  }
+
+  /**
+   * Abort the current request and respond wih the passed value
+   *
+   * @param {any} value
+   *
+   * @memberof Reply
+   */
+  end (value) {
+    if (value instanceof Error) {
+      this.extensionCallback(value)
+    } else {
+      this.extensionCallback(null, value, true)
+    }
+  }
+
+  /**
+   * Runs through all extensions and keep the passed value to respond it
+   *
+   * @param {any} value
+   *
+   * @memberof Reply
+   */
+  send (value) {
+    if (value instanceof Error) {
+      this.extensionCallback(value)
+    } else {
+      this.extensionCallback(null, value)
+    }
+  }
+}
+
+module.exports = Reply
--- packages/hemera/lib/util.js
+++ packages/hemera/lib/util.js
@@ -93,6 +93,45 @@ class Util {
     }
   }
   /**
+   * Executes a series of callbacks and allows to interrupt
+   * as well as to continue with a final value
+   *
+   * @param {Array<Function>} array
+   * @param {Function} method
+   * @param {Function} callback
+   *
+   * @memberOf Extension
+   */
+  static serialWithCancellation (array, method, callback) {
+    if (!array.length) {
+      callback()
+    } else {
+      let i = 0
+
+      const iterate = function () {
+        const done = function (err, value, abort) {
+          if (err) {
+            callback(err)
+          } else if (value && abort) {
+            callback(null, value)
+          } else {
+            i = i + 1
+
+            if (i < array.length) {
+              iterate(value)
+            } else {
+              callback(null, value)
+            }
+          }
+        }
+
+        method(array[i], done)
+      }
+
+      iterate()
+    }
+  }
+  /**
    * Get high resolution time in nanoseconds
    *
    * @static
