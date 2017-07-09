f6e44d465292fcabba9fc945cddfdbfce7f3c1d8
Dustin Deus <deusdustin@gmail.com>
2017-07-09 12:03:59 +0200

    remove async tests from default suite

---
+++ test/hemera/unit/util.node8.js
@@ -0,0 +1,13 @@
+'use strict'
+
+describe('Util', function () {
+  it('Should be able to detect async function', function (done) {
+    const a = HemeraUtil.isAsyncFunction(async function test () {})
+    const b = HemeraUtil.isAsyncFunction(function test () {})
+    const c = HemeraUtil.isAsyncFunction('')
+    expect(a).to.be.equals(true)
+    expect(b).to.be.equals(false)
+    expect(c).to.be.equals(false)
+    done()
+  })
+})
