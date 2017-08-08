5cc60d13a335cc32500ab48f3decc0bcb4db0ceb
Richard Habermann <sittingbool@gmx.de>
2017-08-08 16:24:08 +0200

    Adding typescript definition
    general so far but works well for me

---
+++ packages/hemera-store/index.d.ts
@@ -0,0 +1,30 @@
+export = HemeraStore;
+declare module HemeraStore {
+    class Store {
+        readonly driver: any;
+        readonly options: any;
+
+        constructor(driver:any, options?:any);
+
+        create(...args: any[]);
+
+        exists(...args: any[]);
+
+        find(...args: any[]);
+
+        findById(...args: any[]);
+
+        remove(...args: any[]);
+
+        removeById(...args: any[]);
+
+        replace(...args: any[]);
+
+        replaceById(...args: any[]);
+
+        update(...args: any[]);
+
+        updateById(...args: any[]);
+
+    }
+}
