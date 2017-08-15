74ecb64fd9dd319b3af8ded0ceea11f0354c0fc1
Dustin Deus <deusdustin@gmail.com>
2017-08-15 20:38:41 +0200

    simplify example, update doc

--- examples/basic/full-example.js
+++ examples/basic/full-example.js
@@ -8,40 +8,11 @@ const hemera = new Hemera(nats, {
 })
 
 hemera.ready(() => {
-  /**
-   * Your Implementations
-   */
   hemera.add({
     topic: 'math',
     cmd: 'add'
   }, (req, cb) => {
     cb(null, req.a + req.b)
-  })
-
-  hemera.add({
-    topic: 'math',
-    cmd: 'sub'
-  }, (req, cb) => {
-    cb(null, req.a - req.b)
-  })
-
-  /**
-   * Call them
-   */
-  hemera.act({
-    topic: 'math',
-    cmd: 'add',
-    a: 1,
-    b: 2
-  }, function (err, resp) {
-    hemera.act({
-      topic: 'math',
-      cmd: 'add',
-      a: 1,
-      b: resp
-    }, function (err, resp) {
-      this.log.info(resp, 'Result')
-    })
   })
 
   hemera.act({
