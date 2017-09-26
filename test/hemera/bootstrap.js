b38cb778608336935bee29d3a7d58e394da2ddb8
Dustin Deus <deusdustin@gmail.com>
2017-09-26 21:21:16 +0200

    use yarn to bootstrap lerna because symlink bug in npm, remove nodeVersion in tests, update gitignore and npm ignore

--- test/hemera/bootstrap.js
+++ test/hemera/bootstrap.js
@@ -7,7 +7,6 @@ global.CodecPipeline = require('../../packages/hemera/lib/codecPipeline')
 global.Code = require('code')
 global.Sinon = require('sinon')
 global.HemeraTestsuite = require('hemera-testsuite')
-global.NodeVersion = require('node-version')
 global.expect = global.Code.expect
 global.UnauthorizedError = Hemera.createError('Unauthorized')
 
