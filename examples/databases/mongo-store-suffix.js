e9296e7246961fec21a8d6120c260c63d9d6a4a1
Dustin Deus <deusdustin@gmail.com>
2017-10-18 20:52:12 +0200

    update dev deps

--- examples/databases/mongo-store-suffix.js
+++ examples/databases/mongo-store-suffix.js
@@ -5,29 +5,29 @@ const nats = require('nats').connect()
 const hemeraMongo = require('./../../packages/hemera-mongo-store')
 
 const hemera = new Hemera(nats, {
-    logLevel: 'info',
-    childLogger: true
+  logLevel: 'info',
+  childLogger: true
 })
 
 hemera.use(hemeraMongo, {
-    useDbAsTopicSuffix: true,
-    mongo: {
-        url: 'mongodb://localhost:27017/testDb'
-    }
+  useDbAsTopicSuffix: true,
+  mongo: {
+    url: 'mongodb://localhost:27017/testDb'
+  }
 })
 
-
 hemera.ready(() => {
-
-    hemera.act({
-        topic: 'mongo-store.testDb',
-        cmd: 'create',
-        collection: 'collTest',
-        data: {
-            name: 'peter'
-        }
-    }, function (err, resp) {
-        this.log.info(resp, 'Query result')
-    });
-
+  hemera.act(
+    {
+      topic: 'mongo-store.testDb',
+      cmd: 'create',
+      collection: 'collTest',
+      data: {
+        name: 'peter'
+      }
+    },
+    function(err, resp) {
+      this.log.info(resp, 'Query result')
+    }
+  )
 })
