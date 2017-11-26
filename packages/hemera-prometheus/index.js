287d0144ea9671930c25a7ed020e86ff077794ba
Dustin Deus <deusdustin@gmail.com>
2017-11-26 11:44:38 +0100

    [ci skip] improve prometheus client

--- packages/hemera-prometheus/index.js
+++ packages/hemera-prometheus/index.js
@@ -24,20 +24,27 @@ function hemeraPrometheus(hemera, opts, done) {
   if (opts.collectDefaultMetrics) {
     // Enable collection of default metrics
     Prom.collectDefaultMetrics()
+    // Expose default metrics on public http endpoint
+    server.get('/metrics', (req, res) => {
+      res.set('Content-Type', Prom.register.contentType)
+      res.end(Prom.register.metrics())
+    })
   }
 
   // Expose a single metric on http endpoint
   hemera.decorate('exposeMetric', metric => {
+    if (typeof metric !== 'string') {
+      const err = new Error(
+        `Metric name must be from type string but got ${typeof metric}`
+      )
+      hemera.log.error(err)
+      throw err
+    }
+
     server.get('/metrics/' + metric, (req, res) => {
       res.set('Content-Type', Prom.register.contentType)
       res.end(Prom.register.getSingleMetricAsString(metric))
     })
-  })
-
-  // Expose default metrics on http endpoint
-  server.get('/metrics', (req, res) => {
-    res.set('Content-Type', Prom.register.contentType)
-    res.end(Prom.register.metrics())
   })
 
   // Gracefully shutdown
