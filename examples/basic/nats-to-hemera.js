dcee383b07b62bb39ef24511d44a4c8c670fa53e
starptech <deusdustin@gmail.com>
2018-08-02 21:57:25 +0200

    [ci skip] Add example to communicate from different nats driver to hemera

---
+++ examples/basic/nats-to-hemera.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Hemera = require('./../../packages/hemera')
+const nats = require('nats').connect()
+
+const hemera = new Hemera(nats, {
+  logLevel: 'info'
+})
+
+/**
+ * This is useful if you want to communicate with hemera from other clients e.g Golang, Python.
+ * The Apcera team maintains clients written in Python, Ruby, Node.js, Elixir, Java, NGINX, C, and C#
+ * More informations https://github.com/nats-io/node-nats
+ */
+
+hemera.ready(() => {
+  // Receive in NodeJs client
+  hemera.add(
+    {
+      topic: 'math'
+    },
+    function(req) {
+      hemera.log.info(req)
+    }
+  )
+  // Publish from Golang client
+  hemera.transport.publish(
+    'math',
+    JSON.stringify({
+      request: { type: 'request' },
+      pattern: { topic: 'math', a: 1, b: 2 }
+    })
+  )
+})
