c997509a521863289672f275e870189a15ea4f4e
starptech <deusdustin@gmail.com>
2018-10-06 11:18:52 +0200

    add test, update doc

--- test/hemera/plugin-timeout.spec.js
+++ test/hemera/plugin-timeout.spec.js
@@ -42,6 +42,38 @@ describe('Plugin timeout', function() {
     })
   })
 
+  it('Should be able to get the name of the plugin which produce the timeout', function(done) {
+    const nats = require('nats').connect(authUrl)
+
+    const hemera = new Hemera(nats, {
+      pluginTimeout: 100
+    })
+
+    // Plugin
+    let myPlugin = Hp(
+      function myPlugin(hemera, options, done) {
+        // done()
+      },
+      {
+        name: 'myPlugin',
+        options: { a: 1 }
+      }
+    )
+
+    hemera.use(myPlugin)
+
+    hemera.ready(err => {
+      expect(err).to.be.exists()
+      expect(err.code).to.be.equals('ERR_AVVIO_PLUGIN_TIMEOUT')
+      expect(err.fn.name).to.be.equals('myPlugin')
+      expect(err.fn[Symbol.for('plugin-meta')]).to.be.equals({
+        name: 'myPlugin',
+        options: { a: 1 }
+      })
+      hemera.close(done)
+    })
+  })
+
   it('Should not produce an error when timeout is zero', function(done) {
     const nats = require('nats').connect(authUrl)
 
